e03c56b4a8f74ec69fbb6f3543ec69fab086a6c8
ppz1 <133365100+pedrowAguilera@users.noreply.github.com>
2024-04-12 17:03:41 -0300

    Initial commit

---
+++ Exercicio2/Exercicio2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+
+//ATIVIDADE 1
+//int main(int argc, char* argv[]) {
+//	int quantidade = 22;
+//	float preco = 1.50;
+//	float preco2 = 1.25;
+//
+//	double valor1 = quantidade * preco;
+//	double valor2 = quantidade * preco2;
+//
+//	if (quantidade <= 25) {
+//		cout << "Valor a ser pago: ";
+//		cout << valor1;
+//	}
+//	else if (quantidade > 25) {
+//		cout << "Valor a ser pago: ";
+//		cout << valor2;
+//	}
+//	
+//	return 0;
+//}
+
+//ATIVIDADE 2
+//int main(int argc, char* argv[]) {
+//	double variavel = 17;
+//			
+//	double calculo1 = variavel + 10 * 2;
+//	double calculo2 = variavel + 9 * 3 - 8;
+//
+//	if (variavel > 10) {
+//		cout << calculo1;
+//	}
+//	else {
+//		cout << calculo2;
+//	}
+// return 0;
+//}
+
+//ATIVIDADE 3
+//int main(int argc, char* argv[]) {
+//	int numero = -3;
+//
+//	if (numero >= 0) {
+//		cout << "Esse numero e positivo";
+//	}
+//	else {
+//		cout << "Esse numero e negativo";
+//	}
+//
+//	return 0;
+//}
+
+//ATIVIDADE 4
+//int main(int argc, char* argv[]) {
+//	int variavel1 = 150;
+//	int variavel2 = 15;
+//
+//	if (variavel1 > variavel2) {
+//		cout << "O primeiro numero e maior";
+//	}
+//	else if (variavel2 > variavel1) {
+//		cout << "O segundo numero e maior";
+//	}
+//	else if (variavel1 = variavel2) {
+//		cout << "Os dois numeros sao iguais";
+//	}
+//
+//	return 0;
+//}
+
+//ATIVIDADE 5
+int main(int argc, char* argv[]) {
+	double nota1 = 7;
+	double nota2 = 10;
+
+	double media = (nota1 + nota2) / 2;
+
+	if (media >= 7) {
+		cout << "Aprovado!";
+	}
+	else {
+		cout << "Reprovado";
+	}
+
+	return 0;
+}
